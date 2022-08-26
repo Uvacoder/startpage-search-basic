9468c9e2f9fa7a7916b0411219380e9768da0217
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2022-08-26 13:35:28 -0400

    Update Config.js

--- src/Config.js
+++ src/Config.js
@@ -2,7 +2,7 @@
 
 
 const config = {
-  'search-engine':'duckduckgo', // currently supported options: google, duckduckgo
+  'search-engine':'google', // currently supported options: google, duckduckgo
   // to add a new search engine, go into Search.js and add a new engine.
   'placeholder':'Search',
   'icon':'Bot' // The first letter in this should be caps. Using lucide icons: https://lucide.dev
